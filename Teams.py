1ee074849fa1445430b010464be2271079fdf86c
DataWannaSmile <maxbukh102938@gmail.com>
2025-10-20 18:34:11 +0400

    Update Teams.py

--- Teams.py
+++ Teams.py
@@ -3,19 +3,19 @@ import numpy as np
 from datetime import datetime, timedelta
 import random
 
-# Настройки
-np.random.seed(42)  # для воспроизводимости
+
+np.random.seed(42)  
 NUM_MATCHES = 30000
 NUM_TEAMS = 10
 
-# Список команд (указанные команды)
+
 teams = [
     "Team Spirit", "Gaimin Gladiators", "Team Liquid", "BetBoom Team", 
     "Azure Ray", "Xtreme Gaming", "Shopify Rebellion", "Tundra Esports", 
     "Parivision", "Heroic"
 ]
 
-# Создаем таблицу команд
+
 teams_table = pd.DataFrame({
     'Team_ID': range(1, NUM_TEAMS + 1),
     'Team_Name': teams,
@@ -23,9 +23,6 @@ teams_table = pd.DataFrame({
     
 })
 
-print("Таблица команд:")
-print(teams_table.to_string(index=False))
 
 teams_df=pd.DataFrame(teams_table)
 
-teams_df.to_csv(r'C:\Users\maxbu\OneDrive\Desktop\Проекты\Аналитика\Проект 2\dota2_teams.csv', index=False, encoding='utf-8-sig')
